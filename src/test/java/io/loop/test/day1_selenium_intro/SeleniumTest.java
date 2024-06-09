0e669ebb3dbe66b8cae1d6e4cc107df48fc94c06
naimz <sameernaimzadeh@gmail.com>
2024-06-09 06:45:25 -0700

    My first commit

--- src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/io/loop/test/day1_selenium_intro/SeleniumTest.java
@@ -1,5 +1,36 @@
 package io.loop.test.day1_selenium_intro;
 
-public class SeleniumTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class SeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        // setting up the web driver
+        //WebDriverManager.chromedriver().setup(); but we do not need it with selenium 4.212
+        //
+        WebDriver driver = new ChromeDriver();
+        //maximize the window
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        //navigate to the web page
+        driver.get("https://www.loopcamp.io");
+        driver.navigate().to("https://etsy.com");
+
+        //navigate back
+        driver.navigate().back();
+
+        //forward
+        driver.navigate().forward();
+
+        //refresh
+        driver.navigate().refresh();
+        //quit close
+        // driver.close();
+        driver.quit();
+
+
+
+    }
 }
